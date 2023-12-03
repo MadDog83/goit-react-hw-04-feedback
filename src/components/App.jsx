2839aa0777c323a0673dea6c74ddfe381e54c7cf
MadDog83 <130376768+MadDog83@users.noreply.github.com>
2023-12-03 12:00:22 +0100

    add/App with hucks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,62 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback } from 'react';
 import Feedback from './Feedback/Feedback';
 import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleFeedback = useCallback((type) => {
+    switch (type) {
+      case 'good':
+        setGood((prevGood) => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral((prevNeutral) => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad((prevBad) => prevBad + 1);
+        break;
+      default:
+        break;
+    }
+  }, []);
+
+  const countTotalFeedback = useCallback(() => good + neutral + bad, [good, neutral, bad]);
+
+  const countPositiveFeedbackPercentage = useCallback(() => (good / countTotalFeedback()) * 100, [good, countTotalFeedback]);
+
+  const centerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh'
   };
 
-  handleFeedback = (type) => {
-    this.setState((prevState) => ({
-      [type]: prevState[type] + 1
-    }));
-  };
+  return (
+    <div style={centerStyle}>
+      <Section title="Please leave feedback">
+        <Feedback options={['good', 'neutral', 'bad']} onLeaveFeedback={handleFeedback} />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
+};
 
-  countTotalFeedback = () => this.state.good + this.state.neutral + this.state.bad;
-
-  countPositiveFeedbackPercentage = () => (this.state.good / this.countTotalFeedback()) * 100;
-  render() {
-    const centerStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh'
-    };
-
-    return (
-        <div style={centerStyle}>
-            <Section title="Please leave feedback">
-                <Feedback options={['good', 'neutral', 'bad']} onLeaveFeedback={this.handleFeedback} />
-            </Section>
-            <Section title="Statistics">
-                {this.countTotalFeedback() > 0 ? (
-                    <Statistics
-                        good={this.state.good}
-                        neutral={this.state.neutral}
-                        bad={this.state.bad}
-                        total={this.countTotalFeedback()}
-                        positivePercentage={this.countPositiveFeedbackPercentage()}
-                    />
-                ) : (
-                    <Notification message="There is no feedback" />
-                )}
-            </Section>
-        </div>
-    );
-  } 
-}
+export default App;
